79fbfbe57fc753c2581146fa6a0e6bccd6b33fbe
Sai Sashreek D <saisashreekd@gmail.com>
2024-11-16 15:20:26 +0530

    Font Size Updated

--- components/Chapters.jsx
+++ components/Chapters.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function Chapters({ chapterNum, chapterTitle, chapterDesc, onChapterSelct, bgGrad }) {
   return (
     <>
-      <div className="flex flex-col gap-3 homeTextBold">
+      <div className="flex flex-col gap-6 homeTextBold">
         <div
-          className="flex flex-col gap-2 cursor-pointer"
+          className="flex flex-col gap-4 cursor-pointer"
           onClick={onChapterSelct}
         >
-          <h1 className="text-2xl text-slate-600 font-medium">{chapterNum}</h1>
-          <h1 className="text-5xl text-slate-600 font-medium">
+          <h1 className="text-4xl text-slate-600 font-medium">{chapterNum}</h1>
+          <h1 className="text-6xl text-slate-600 font-medium">
             {chapterTitle}
           </h1>
-          <h1 className="text-4xl text-slate-600 font-medium">{chapterDesc}</h1>
+          <h1 className="text-5xl text-slate-600 font-medium">{chapterDesc}</h1>
         </div>
         <div className={`w-full h-1 shadow-lg rounded-lg ${bgGrad}`}></div>
       </div>
