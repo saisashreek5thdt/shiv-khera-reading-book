59dfc92ffa541dd5284179a2c6e6b3696df87e98
Sai Sashreek D <saisashreekd@gmail.com>
2024-11-18 17:39:01 +0530

    Content Updated

--- app/pages/eng/winner/page.jsx
+++ app/pages/eng/winner/page.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
 
-function Winner() {
+function EngWinner() {
+
+  const router = useRouter();
+
+  const nextPageHandler = (e) => {
+    e.preventDefault();
+    router.push("/pages/eng/ability");
+  };
+
   return (
-    <> 
-        <div className='w-full min-h-screen h-full chapterBG'></div>
+    <>
+      <div className="w-full chapterBG overflow-hidden">
+        <div className="h-screen flex flex-col gap-4 items-center justify-center homeTextBold font-medium">
+          <h1 className="text-5xl text-slate-700">Importance of Attitude</h1>
+          <h1 className="text-5xl text-slate-700">Winner&apos;s Edge</h1>
+          <h1 className="pt-10 text-5xl text-slate-600 homeText font-medium cursor-pointer" onClick={nextPageHandler}>Next</h1>
+        </div>        
+      </div>
     </>
-  )
+  );
 }
 
-export default Winner
+export default EngWinner;
