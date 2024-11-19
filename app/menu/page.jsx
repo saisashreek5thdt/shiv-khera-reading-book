28b2147c2d3c4e79f22e21ce9cd664f89441bc4d
Sai Sashreek D <saisashreekd@gmail.com>
2024-11-19 15:14:12 +0530

    New Layout Added

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -6,17 +6,16 @@ import FooterLang from "@/components/FooterLang";
 import { useRouter } from "next/navigation";
 
 function Menu() {
-
   const router = useRouter();
 
   const langPageHandler = (e) => {
     e.preventDefault();
     router.push("/");
-  }
+  };
 
   return (
     <>
-      <div className="min-h-screen bg-[#e1dcd8] menuBG">
+      {/* <div className="min-h-screen bg-[#e1dcd8] menuBG">
         <div className="grid grid-cols-1 gap-4 sm:order-2 lg:grid-cols-5 lg:gap-2">
           <div className="h-screen rounded-lg lg:col-span-2">
             <Image
@@ -27,6 +26,25 @@ function Menu() {
             />
           </div>
           <div className="h-screen rounded-lg sm:order-1 lg:col-span-3">
+            <Language />
+          </div>
+        </div>
+      </div> */}
+      <div className="bg-[#e1dcd8] menuBG h-screen flex">
+        {/* Fixed Image Component */}
+        <div className="w-1/3 relative">
+          <div className="fixed top-0 left-0 w-1/3 h-screen">
+            <Image
+              src={"/menuImg.png"} // Replace with your image path
+              alt="Fixed Example"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </div>
+        {/* Scrollable Content Component */}
+        <div className="w-2/3 overflow-y-scroll p-3">
+          <div className="space-y-8">
             <Language />
           </div>
         </div>
