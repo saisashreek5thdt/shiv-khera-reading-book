8a24ae938ae5b7953d52ace6557a3f7c614db8be
Sai Sashreek D <saisashreekd@gmail.com>
2024-11-21 12:35:10 +0530

    Custom Animations Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
         slideIn: "slideIn 1.5s ease-in-out 2s",
         slideInLeft: "slide-in-left 0.5s ease-in 2s both",
         bottomTop: "bottomTop 0.5s easeInOutQuad 1.8s both",
+        typeWriter1: "typeWriter1 8s steps(8) 0.5s 1 normal both, cursorBlink 0.5s infinite",
+        typeWriter2: "typeWriter2 8s steps(5) 0.5s 1 normal both, cursorBlink 0.5s infinite",
+        cursorBlink: "cursorBlink 0.5s infinite"
       },
       keyframes: {
         fadeIn: {
@@ -42,6 +45,32 @@ module.exports = {
             opacity: "1",
           },
         },
+        typeWriter1: {
+          from: {
+            width: "0"
+          },
+          
+          to: {
+            width: "100%"
+          }
+        },
+        typeWriter2: {
+          from: {
+            width: "0"
+          },
+          
+          to: {
+            width: "40%"
+          }
+        },
+        cursorBlink: {
+          from: {
+            borderRight: "1px solid #dedede"
+          },
+          to: {
+            borderRight: "1px solid #666"
+          }
+        }
       },
       backgroundImage: {
         'chapterBG': "url('../public/chapterBG.png')"
